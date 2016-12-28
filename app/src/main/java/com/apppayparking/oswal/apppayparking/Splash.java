3a1f2f962ef8ab82e5e93e464a27272043203588
Oswaldo Echeverría Suárez <oswaldo_musician@hotmail.com>
2016-12-28 01:46:24 -0500

    Activity de validacion  y ceracion de activity  de menu principal

--- app/src/main/java/com/apppayparking/oswal/apppayparking/Splash.java
+++ app/src/main/java/com/apppayparking/oswal/apppayparking/Splash.java
@@ -1,13 +1,25 @@
 package com.apppayparking.oswal.apppayparking;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Splash extends AppCompatActivity {
+public class Splash extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, Validar.class);
+                startActivity(intent);
+            }
+        }, 4000);
     }
 }
